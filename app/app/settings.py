4ec38c002a78ca9c36b0f305a4489916e670690b
Илья Немкин <nemkin@garpix.com>
2025-01-17 19:39:56 +0300

    fsdfds

--- app/app/settings.py
+++ app/app/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = []
+STATIC_ROOT = BASE_DIR /'..' / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
